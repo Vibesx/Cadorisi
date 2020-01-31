631495b472451ccb07c2f028046b939dba58666f
lucian.paslariu@cgm.com <lucian.paslariu@cgm.com>
2020-01-31 17:28:07 +0200

    first commit

---
+++ cadorisi/src/main/java/com/cadorisi/cadorisi/CadorisiApplication.java
@@ -0,0 +1,13 @@
+package com.cadorisi.cadorisi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadorisiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadorisiApplication.class, args);
+	}
+
+}
---
+++ cadorisi/src/test/java/com/cadorisi/cadorisi/CadorisiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cadorisi.cadorisi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadorisiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
